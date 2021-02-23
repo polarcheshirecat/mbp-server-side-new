2e5105c16ba86a2365d427b9edf5ba254501b533
Yuriy <ybojko@yandex.ru>
2021-02-23 16:48:55 +0300

    Добавлена поддержка ELK

--- src/main/java/ru/spb/devpractices/masterbest/serverside/ServerSideApplication.java
+++ src/main/java/ru/spb/devpractices/masterbest/serverside/ServerSideApplication.java
@@ -1,5 +1,7 @@
 package ru.spb.devpractices.masterbest.serverside;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +10,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 import ru.spb.devpractices.masterbest.serverside.mappers.IntegrationPushOrderMapper;
 
+import java.net.InetAddress;
+
+@Slf4j
 @SpringBootApplication
 public class ServerSideApplication {
+
+    @Value("${local.server.port}")
+    private static int serverPort;
+
+
 
     @Bean
     IntegrationPushOrderMapper getMapper (){
@@ -22,10 +32,9 @@ public class ServerSideApplication {
     }
 
     public static void main(String[] args) {
-
+        log.info("Приложение запущено на " +
+                InetAddress.getLoopbackAddress().getHostAddress()
+                + ":" + serverPort);
         SpringApplication.run(ServerSideApplication.class, args);
     }
-
-
-
 }
