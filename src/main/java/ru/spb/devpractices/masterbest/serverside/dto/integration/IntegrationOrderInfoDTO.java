8a4e8f0ce2e3bcfcf3f73148a168ae6a8d43e1c9
Yuriy <ybojko@yandex.ru>
2021-03-09 19:23:49 +0300

    Отладка обмена пушами с 1С, исправления

--- src/main/java/ru/spb/devpractices/masterbest/serverside/dto/integration/IntegrationOrderInfoDTO.java
+++ src/main/java/ru/spb/devpractices/masterbest/serverside/dto/integration/IntegrationOrderInfoDTO.java
@@ -1,6 +1,7 @@
 package ru.spb.devpractices.masterbest.serverside.dto.integration;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -9,5 +10,5 @@ import java.io.Serializable;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class IntegrationOrderInfoDTO implements Serializable {
     private String numberMB;
-    private String ikeaNumber;
+    private String numberIkea;
 }
